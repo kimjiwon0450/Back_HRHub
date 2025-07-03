9847a7872f1b11cd1d99ebad49b0437b7fb8220d
hajoon <stravinskky@naver.com>
2025-07-03 14:53:21 +0900

    YHJ 부서 등록

--- hr-service/src/main/java/com/playdata/hrservice/hr/controller/DepartmentController.java
+++ hr-service/src/main/java/com/playdata/hrservice/hr/controller/DepartmentController.java
@@ -2,6 +2,7 @@ package com.playdata.hrservice.hr.controller;
 
 import com.playdata.hrservice.common.auth.JwtTokenProvider;
 import com.playdata.hrservice.common.dto.CommonResDto;
+import com.playdata.hrservice.hr.dto.DepartmentReqDto;
 import com.playdata.hrservice.hr.dto.DepartmentResDto;
 import com.playdata.hrservice.hr.entity.Department;
 import com.playdata.hrservice.hr.service.DepartmentService;
@@ -10,10 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hr-service")
@@ -38,5 +36,11 @@ public class DepartmentController {
                 HttpStatus.OK, "Success", departmentService.getAllDepartments()
         );
         return new ResponseEntity<>(resDto, HttpStatus.OK);
+    }
+
+    @PostMapping("/department/add")
+    public ResponseEntity<?> createDepartment(@RequestBody DepartmentReqDto dto) {
+        departmentService.createDepartment(dto);
+        return ResponseEntity.ok().build();
     }
 }
---
+++ hr-service/src/main/java/com/playdata/hrservice/hr/dto/DepartmentReqDto.java
@@ -0,0 +1,12 @@
+package com.playdata.hrservice.hr.dto;
+
+import lombok.*;
+
+@Builder
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class DepartmentReqDto {
+    private String name;
+}
